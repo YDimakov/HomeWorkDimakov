3145aa0d86b4aa0355fadfe171ad6cd26562920e
YDimakov <66439632+YDimakov@users.noreply.github.com>
2020-06-07 15:17:10 +0300

    HomeWork8
    Added my work8

---
+++ HomeWork8/Work/src/menu/PathFile.java
@@ -0,0 +1,9 @@
+package menu;
+
+public class PathFile {
+    public static final String OUTPUT_ZIP_FILE = "C:\\Users\\YD\\IdeaProjects\\BackUp\\Folder.zip";
+    public static final String SOURCE_FOLDER_TAKE = "C:\\Users\\YD\\IdeaProjects\\Work\\src";
+    public static final String SOURCE_FOLDER2_GIVE = "C:\\Users\\YD\\IdeaProjects\\Work";
+    public static final String HISTORY_WORK = "C:\\Users\\YD\\IdeaProjects\\Work\\src\\historyWork.txt";
+//    public static final String ROOT_DIR_PATH = "C:\\Users\\YD\\IdeaProjects\\Work\\src\\rootDir";
+}
