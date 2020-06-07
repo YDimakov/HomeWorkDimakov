9305e0c325bad8f03e70cc449148090005e325ef
YDimakov <66439632+YDimakov@users.noreply.github.com>
2020-06-07 15:05:06 +0300

    HomeWork3
    Added my work3

---
+++ HomeWork3/ActionСalculator.java
@@ -0,0 +1,20 @@
+package HomeWork3;
+
+public class ActionСalculator {
+    
+    static double addition(double a, double b) {
+        return a + b;
+    }
+
+    static double difference(double a, double b) {
+        return a - b;
+    }
+
+    static double multiplication(double a, double b) {
+        return a * b;
+    }
+
+    static double division(double a, double b) {
+        return a / b;
+    }
+}
